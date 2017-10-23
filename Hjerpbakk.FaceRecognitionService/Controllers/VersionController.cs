d0000b9ed8f823a75fd190220bbe9b27beeef8e8
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-10-23 13:41:05 +0200

    Added version API

---
+++ Hjerpbakk.FaceRecognitionService/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hjerpbakk.FaceRecognitionService.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
